3e9416fa95b3051177b580cdc666bc6c4d35eb00
Lucas Vitor <lucasvitorfc@gmail.com>
2023-09-16 12:38:06 -0300

    Deletei os arquivos

--- responsive.js
+++
@@ -1,9 +0,0 @@
-function menu() 
-{
-    var x = document.getElementById("navbar-links");
-    if (x.className === "navbar-links") {
-      x.className += " responsive";
-    } else {
-      x.className = "navbar";
-    }
-}
